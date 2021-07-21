eb224ffd8ecd684cf3c27a51136e24457cc69a37
Nataly <natasha-vd@yandex.ru>
2021-07-21 11:18:59 +0300

    Импортировала правильно картинку

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import './scss/App.scss';
 import {Message} from './components/Message';
+import user1 from './img/user-1.jpg';
 
 const userName = 'Ксюша';
 const textUser = 'Привет. Как дела?';
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="app container">
       <div className="app__item">
-        <img className="app__user-img" src="../img/user-1.jpg" alt="user1" width="50" height="50"/>
+        <img className="app__user-img" src={user1} alt="user1" width="50" height="50"/>
         <div>
           <p className="app__user-text">
             {userName}
