b6accb01c0ed10b76bed2190fb320467d36e8994
Nataly <natasha-vd@yandex.ru>
2021-07-22 12:14:15 +0300

    Выполнение ДЗ-2

--- my-app/src/components/Message.js
+++ my-app/src/components/Message.js
@@ -1,7 +1,17 @@
+import '../scss/message.scss';
+
 export const Message = (props) => {
     return(
-        <p className="message-user">
-            {props.text}
-        </p>
+        <li className="message">
+            <img className="message__user-img" src={props.message.photo} alt="user1" width="50" height="50" />
+            <div>
+                <p className="message__user">
+                    {props.message.author}
+                </p>
+                <p>
+                    {props.message.text}
+                </p>
+            </div>
+        </li>
     )
 }
